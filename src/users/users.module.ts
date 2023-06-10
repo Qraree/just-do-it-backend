675d3d8854f8b3357972469617ff0d693ce8ea38
Qraree <metbezu@gmail.com>
2023-06-10 14:54:17 +0300

    task fix

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,11 +3,12 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { User } from './entities/user.entity';
+import { Task } from '../tasks/entities/task.entity';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
-  imports: [SequelizeModule.forFeature([User])],
+  imports: [SequelizeModule.forFeature([User, Task])],
   exports: [UsersService],
 })
 export class UsersModule {}
